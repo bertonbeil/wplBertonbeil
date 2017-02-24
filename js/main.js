ecdf58d99b2a0439521daa4a07f868190f09ab7c
Andrew Dmytriv <skateboardsmart@gmail.com>
2017-02-24 03:22:48 +0200

    create linear bg pop-up animation

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ $(window).bind('scroll', function () {
 this.body = $('body');
 this.modal = $('#modalMore');
 this.modalBtn = $('#moreBtn');
-this.closeBtn = $('a.close');
+this.closeBtn = $('a#close');
 
 
 modalBtn.on('click',  function () {
@@ -30,7 +30,6 @@ modalBtn.on('click',  function () {
   } else {
     openModal();
   }
-
   closeBtn.on('click', function () {
     closeModal();
   });
@@ -38,12 +37,46 @@ modalBtn.on('click',  function () {
 
 
 function openModal() {
+  lineMakerModal.animateLinesIn();
   body.addClass('open-modal');
   modal.addClass('open');
+  console.log('open');
 }
 function closeModal() {
+  lineMakerModal.animateLinesOut( {duration: 200} );
   body.removeClass('open-modal');
   modal.removeClass('open');
+  console.log('close');
 }
 
-console.log(modalBtn);
+
+var lineMaker = new LineMaker({
+  parent: {
+    element: '#svg-container',
+    position: 'absolute'
+  },
+  lines: [
+    {top: 0, left: '0%', width: 1, height: '100vh', color: '#CDCFD0', hidden: true, animation: { duration: 1000, easing: 'easeInOutSine', delay: 400, direction: 'TopBottom' }},
+    {top: 0, left: '25%', width: 1, height: '100vh', color: '#CDCFD0', hidden: true, animation: { duration: 1000, easing: 'easeInOutQuad', delay: 100, direction: 'BottomTop' }},
+    {top: 0, left: '50%', width: 1, height: '100vh', color: '#CDCFD0', hidden: true, animation: { duration: 500, easing: 'easeInOutQuad', delay: 0, direction: 'BottomTop' }},
+    {top: 0, left: '75%', width: 1, height: '100vh', color: '#CDCFD0', hidden: true, animation: { duration: 1000, easing: 'easeOutSine', delay: 400, direction: 'TopBottom' }},
+    {top: 0, left: '100%', width: 1, height: '100vh', color: '#CDCFD0', hidden: true, animation: { duration: 800, easing: 'easeOutSine', delay: 300, direction: 'BottomTop', color2: '#000' }}
+  ]
+});
+
+var lineMakerModal = new LineMaker({
+  parent: {
+    element: '#modalMore',
+    position: 'absolute'
+  },
+  lines: [
+    {top: 0, left: '0%', width: '25%', height: '100vh', color: '#097cf3', hidden: true, animation: { duration: 300, easing: 'easeInOutSine', delay: 100, direction: 'TopBottom' }},
+    {top: 0, left: '25%', width: '25%', height: '100vh', color: '#097cf3', hidden: true, animation: { duration: 300, easing: 'easeInOutQuad', delay: 50, direction: 'TopBottom' }},
+    {top: 0, left: '50%', width: '25%', height: '100vh', color: '#097cf3', hidden: true, animation: { duration: 300, easing: 'easeInOutQuad', delay: 150, direction: 'TopBottom' }},
+    {top: 0, left: '75%', width: '25%', height: '100vh', color: '#097cf3', hidden: true, animation: { duration: 300, easing: 'easeOutSine', delay: 0, direction: 'TopBottom' }}
+  ]
+});
+
+setTimeout(function() {
+  lineMaker.animateLinesIn();
+}, 250);
