947d139e400a029f2a12d84ba1f1bd439bdeabf4
Andrew <skateboardsmart@gmail.com>
2017-03-20 13:52:06 +0200

    navigation, header, menu fixed, new colors

--- js/main.js
+++ js/main.js
@@ -7,43 +7,6 @@ slWork.on( "mouseleave", function(){ slCl.css({transform: "rotate(90deg)"});});
 slMore.on( "mouseenter", function(){ slCl.css({transform: "rotate(125deg)"});});
 slMore.on( "mouseleave", function(){ slCl.css({transform: "rotate(90deg)"});});
 
-// nav scroll
-var jsHeader = $('section.main-cover');
-var $shadower = $('.shadower');
-var jsNav = $('header.header');
-
-$(window).bind('scroll', function () {
-    var hW = $(jsHeader).outerHeight();
-    var wScroll = $(this).scrollTop();
-    var navTop = $(jsNav).height();
-    var headScroll = (wScroll / 2);
-    var faderScroll = ( wScroll / hW );
-    var fadeToColor = Math.min(10, faderScroll);
-
-
-
-
-    // console.log(wScroll);
-    console.log(wScroll);
-
-    if ( navTop > 100 || wScroll < 500) {
-      console.log(wScroll);
-      jsNav.css({
-        'background': "rgba(0, 0, 0, " + fadeToColor * 2 + " )",
-        'height': 200 - (fadeToColor * 180),
-        'line-height': 200 - (fadeToColor * 180) + 'px',
-        'top': 150 - (fadeToColor * 260)
-      });
-    }
-    if ( navTop < 100 || wScroll > 500 ) {
-      console.log(wScroll);
-      jsNav.css({
-        'top': 0,
-        'height': 100,
-        'line-height': '100px'
-      });
-    }
-});
 
 // modal window
 this.body = $('body');
@@ -58,9 +21,6 @@ this.moreLinks = $('.more-links');
 this.socialLinks = $('.social-list');
 
 this.hambBtn = $('button.menu-hamb');
-
-
-
 
 
 var bb = $('#hamburger');
@@ -163,59 +123,3 @@ portItem.hover(
         $(this).removeClass('hover');
     }
 );
-
-
-// #######
-function scrollFooter(scrollY, heightFooter) {
-    if (scrollY >= heightFooter) {
-        $('footer').css({
-            'bottom': '0px'
-        });
-    } else {
-        $('footer').css({
-            'bottom': '-' + heightFooter + 'px'
-        });
-    }
-}
-
-$(window).ready( function() {
-    var windowHeight        = $(window).height(),
-        footerHeight        = $('footer').height(),
-        heightDocument      = (windowHeight) + ($('.content').height()) + ($('footer').height()) + 160;
-
-    // Definindo o tamanho do elemento pra animar
-    $('#scroll-animate, #scroll-animate-main').css({
-        'height' :  heightDocument + 'px'
-    });
-
-    // Definindo o tamanho dos elementos header e conteúdo
-    $('section.main-cover').css({
-        'height' : windowHeight + 'px',
-        'line-height' : windowHeight + 'px'
-    });
-
-    $('.wrapper-parallax').css({
-        'margin-top' : windowHeight + 'px'
-    });
-
-    scrollFooter(window.scrollY, footerHeight);
-
-    // ao dar rolagem
-    window.onscroll = function(){
-        var scroll = window.scrollY;
-
-        $('#scroll-animate-main').css({
-            'top' : '-' + scroll + 'px'
-        });
-
-        // $('section.main-cover').css({
-        //     'background-position-y' : 50 + (scroll * 1000 / heightDocument) + '%'
-        // });
-
-        // $('#bgvid').css({
-        //     'transform' : 'translateX(-50%) translateY(-' + 5 + (scroll * 10 / heightDocument) + '%' + ')'
-        // });
-
-        scrollFooter(scroll, footerHeight);
-    }
-});
